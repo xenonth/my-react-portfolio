c4f5cfcbd8c54e9853ae6669f8a649ded0e1c3ed
Timothy Forbes <timothy.forbes259@gmail.com>
2021-03-04 14:44:13 +1030

    functioning header bar using material UI.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+// imported for user authentication
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+//import page files 
+import Home from './pages/Home';
+
+
+//components which sit outside the main page
+import Header from './components/Header';
+
+
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+        <Route exact path ="/" component={Home} />
+        <Route exact path ="/home" component={Home} />
+
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h2>Welcome User</h2>
+        </div>
+    )
+}
+
+export default Home;
